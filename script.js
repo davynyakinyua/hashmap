8bdbbaef74cfc7716c6d2647fc501309fedc6633
Davy Nyakinyua <davynyakingua@gmail.com>
2025-08-19 10:29:54 +0300

    Add get, has and remove methods

--- script.js
+++ script.js
@@ -26,15 +26,49 @@ class Hashing {
     // altering values of keys
     set (key, value){
         // check for collision
-        
+
         // hash the key
         let hashedKey = this.hash(key);
 
         // push the hash to hashKeys array
-        this.hashedKeys.push(hashedKey);
+        if(!this.has(key)){
+            this.hashedKeys.push(hashedKey);
+        }
 
         // add the hashed key and value to the items object
          return this.items[hashedKey] = value;
+    }
+
+    // get key method returns the value of the key
+    get (key){
+        if(!key){
+            return null;
+        }
+        hash = this.hash(key);
+
+        if(!hash in this.items) return null;
+
+        return this.items[hash];
+    }
+
+    // has method checks if the key exist 
+    has (key){
+        return key in this.items;
+    }
+
+    // remove method to remove entry
+    remove (key){
+
+        let hashed = this.hash(key);
+        if(this.has(hashed)){
+            // delete entry from object
+            delete this.items[hashed];
+
+            this.hashedKeys = this.hashedKeys.filter(hash => hash !== hashed);
+
+            return true;
+        }
+        return false;
     }
 
 }
